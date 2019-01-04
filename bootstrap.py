9bef94e94db80424ab5655a47d27246da19f3258
Alexander Neshitov <neshitov@usc.edu>
2019-01-03 23:34:48 -0800

    code: add two methods to confidence interval

--- bootstrap.py
+++ bootstrap.py
@@ -10,10 +10,12 @@ class Bootstrap:
         self.computed = False
         self.distribution = None
         if not callable(self.stat):
+            assert self.stat in ['mean', 'std'], 'stat must be callable or one \
+                                                  of "mean", "std" '
             self.stat = {'mean': np.mean, 'std': np.std}[self.stat]
         if self.sample_size is None:
             self.sample_size = sample.shape[0]
-        
+
     def get_distribution(self):
         if not self.computed:
             self.distribution = np.zeros(self.n_iter)
@@ -24,6 +26,13 @@ class Bootstrap:
             self.computed = True
         return self.distribution
 
-    def get_confidence_interval(self, alpha):
+    def get_confidence_interval(self, alpha, method = 't_percentile'):
         self.distribution = self.get_distribution()
-        return np.percentile(self.distribution, [(100-alpha)/2, 100-(100-alpha)/2])
+        if method == 't_percentile':
+            theta = self.stat(self.sample)
+            sigma = np.std(self.distribution)
+            xi_left, xi_right = np.quantile((self.distribution - theta)/(sigma + 10**(-8)),
+                                            [alpha/2, 1-alpha/2])
+            return [theta - xi_right * sigma, theta - xi_left * sigma]
+        if method == 'efron':
+            return np.quantile(self.distribution, [alpha/2, 1-alpha/2])
