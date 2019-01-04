a1c5e4c738f551f5ac937c8ded46c94b4ee59603
Alexander Neshitov <neshitov@usc.edu>
2019-01-03 19:51:06 -0800

    up

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from bootstrap import Bootstrap
+import progressbar
 import time
 
 N=1000
@@ -9,9 +10,9 @@ check=np.zeros(N)
 for i in range(N):
     n_iter=10000
     sample = np.random.randn(10**2)
-    bs = Bootstrap(sample,n_iter=n_iter)
+    bs = Bootstrap(sample, stat = 'std', n_iter = n_iter)
     left, right = bs.get_confidence_interval(95)
-    check[i] = (left < 0) & (right >0)
-
+    check[i] = (left < 1) & (right >1)
+    print('iteration: #%d, interval:(%.5f,%.5f)' %(i,left,right))
 print('after %d iterations got %d correct intervals' %(N,np.sum(check)))
 #plt.show()
