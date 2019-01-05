bc740f09b364ed86e9eec676a3ac200a81a8fc67
Alexander Neshitov <neshitov@usc.edu>
2019-01-04 19:45:44 -0800

    docs: add docstrings to class and method

--- code.py
+++ code.py
@@ -1,31 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from bootstrap import Bootstrap
-import progressbar
-from scipy.stats import norm
-import time
 
-print(10**(-8))
-print(np.version.version)
 np.random.seed(42)
-N=10
-correct=1
-check_t=np.zeros(N)
+N=1000
+# true value of uniform distribution std
+correct=np.sqrt(1/12)
+# Estimate bootsrap confidence intervals for std of uniform distribution
+# of confidence level 95%
+
+# array to store 1 if true value is inside the confidence interval
+check_bc=np.zeros(N)
 check_perc=np.zeros(N)
-check_efron=np.zeros(N)
+check_sampling=np.zeros(N)
+
+# length of confidence intervals
+length_bc=np.zeros(N)
+length_perc=np.zeros(N)
+length_sampling=np.zeros(N)
+
 for i in range(N):
-    n_iter=10000
-    sample = np.random.randn(10**2)
-    bs = Bootstrap(sample, stat = 'std', n_iter = n_iter)
-    left_t, right_t = bs.get_confidence_interval(0.05, method = 'bias_corrected')
+    sample = np.random.uniform(size=10**2)
+    bs = Bootstrap(sample, stat = 'std')
+
+    left_bc, right_bc = bs.get_confidence_interval(0.05, method = 'bias_corrected')
     left_perc, right_perc = bs.get_confidence_interval(0.05, method = 'percentile')
-    left_efron, right_efron = bs.get_confidence_interval(0.05, method = 'efron')
+    left_sampling, right_sampling = bs.get_confidence_interval(0.05, method = 'sampling')
+
+    length_bc[i] = right_bc - left_bc
+    length_perc[i] = right_perc - left_perc
+    length_sampling[i] = right_sampling - left_sampling
+
+    check_bc[i] = (left_bc<correct) & (right_bc>correct)
     check_perc[i] = (left_perc<correct) & (right_perc>correct)
-    check_t[i] = (left_t<correct) & (right_t>correct)
-    check_efron[i] = (left_efron<correct) & (right_efron>correct)
-    print('iteration %d, bias_corrected: (%.3f,%.3f)' %(i,left_t,right_t))
-    print('iteration %d, percentile: (%.3f,%.3f)' %(i,left_perc,right_perc))
-    print('iteration %d, efron: (%.3f,%.3f)' %(i,left_efron,right_efron))
-print('afet %d iterations %d bias_corrected correct' %(N,np.sum(check_t)))
-print('afet %d iterations %d percentile correct' %(N,np.sum(check_perc)))
-print('afet %d iterations %d efron correct' %(N,np.sum(check_efron)))
+    check_sampling[i] = (left_sampling<correct) & (right_sampling>correct)
+
+print('after %d iterations %d bias_corrected correct' %(N,np.sum(check_bc)))
+print('after %d iterations %d percentile correct' %(N,np.sum(check_perc)))
+print('after %d iterations %d sampling correct' %(N,np.sum(check_sampling)))
+
+# compare the lengths of confidence intervals
+plt.subplot(1,3,1).set_title('bias corrected')
+plt.hist(length_bc)
+plt.subplot(1,3,2)
+plt.hist(length_perc).set_title('percentile method')
+plt.subplot(1,3,3)
+plt.hist(length_sampling).set_title('sampling method')
+plt.tight_layout
+plt.show()
