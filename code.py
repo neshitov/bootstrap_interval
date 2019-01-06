0d9ad19c4cb0b2d35d8607c8156f86d2e599c198
Alexander Neshitov <neshitov@usc.edu>
2019-01-05 17:14:44 -0800

    code: code.py fixed

--- code.py
+++ code.py
@@ -1,50 +1,76 @@
+'''
+This is an example of bootstrap_interval module usage.
+In this example we draw 1000 samples of length 100
+from the uniform distribution U(0,1).
+
+For every sample we compute a bootstrap 95%-confidence interval for
+standard deviation using 3 possible methods: smapling, percentile
+and bias corrected.
+
+Then we compute the number of confidence intervals that contain the true
+standard deviation for each of the methods and display the length distribution
+of the confidene intervals.
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
-from bootstrap import Bootstrap
+import bootstrap_interval
+from sys import stdout
 
 np.random.seed(42)
-N=1000
+N = 10
 # true value of uniform distribution std
-correct=np.sqrt(1/12)
+correct_stat = np.sqrt(1 / 12)
 # Estimate bootsrap confidence intervals for std of uniform distribution
 # of confidence level 95%
 
-# array to store 1 if true value is inside the confidence interval
-check_bc=np.zeros(N)
-check_perc=np.zeros(N)
-check_sampling=np.zeros(N)
+# count number of correctly predicted confidence intervals for each method
+check_bc = 0
+check_perc = 0
+check_sampling = 0
 
-# length of confidence intervals
-length_bc=np.zeros(N)
-length_perc=np.zeros(N)
-length_sampling=np.zeros(N)
+# arrays to store lengths of confidence intervals
+length_bc = np.zeros(N)
+length_perc = np.zeros(N)
+length_sampling = np.zeros(N)
 
 for i in range(N):
-    sample = np.random.uniform(size=10**2)
-    bs = Bootstrap(sample, stat = 'std')
+    stdout.write("\r%d samples processed" % i)
+    stdout.flush()
 
-    left_bc, right_bc = bs.get_confidence_interval(0.05, method = 'bias_corrected')
-    left_perc, right_perc = bs.get_confidence_interval(0.05, method = 'percentile')
-    left_sampling, right_sampling = bs.get_confidence_interval(0.05, method = 'sampling')
+    sample = np.random.uniform(size=10**2)
+    bs = bootstrap_interval.Bootstrap(sample, stat='std')
+
+    left_bc, right_bc = bs.get_confidence_interval(
+                        0.05, method='bias_corrected')
+    left_perc, right_perc = bs.get_confidence_interval(
+                            0.05, method='percentile')
+    left_sampling, right_sampling = bs.get_confidence_interval(
+                                    0.05, method='sampling')
 
     length_bc[i] = right_bc - left_bc
     length_perc[i] = right_perc - left_perc
     length_sampling[i] = right_sampling - left_sampling
 
-    check_bc[i] = (left_bc<correct) & (right_bc>correct)
-    check_perc[i] = (left_perc<correct) & (right_perc>correct)
-    check_sampling[i] = (left_sampling<correct) & (right_sampling>correct)
+    check_bc += (left_bc < correct_stat) & (right_bc > correct_stat)
+    check_perc += (left_perc < correct_stat) & (right_perc > correct_stat)
+    check_sampling += (left_sampling <
+                       correct_stat) & (right_sampling > correct_stat)
 
-print('after %d iterations %d bias_corrected correct' %(N,np.sum(check_bc)))
-print('after %d iterations %d percentile correct' %(N,np.sum(check_perc)))
-print('after %d iterations %d sampling correct' %(N,np.sum(check_sampling)))
+stdout.write("\r   ")
+stdout.flush()
+
+print('Among %d samples:' % N)
+print('%d correct intervals constructed using bias_corrected method' % check_bc)
+print('%d correct intervals constructed using percentile method' % check_perc)
+print('%d correct intervals constructed using sampling method' % check_sampling)
 
 # compare the lengths of confidence intervals
-plt.subplot(1,3,1).set_title('bias corrected')
+plt.subplot(1, 3, 1).set_title('bias corrected')
 plt.hist(length_bc)
-plt.subplot(1,3,2).set_title('percentile method')
+plt.subplot(1, 3, 2).set_title('percentile method')
 plt.hist(length_perc)
-plt.subplot(1,3,3).set_title('sampling method')
+plt.subplot(1, 3, 3).set_title('sampling method')
 plt.hist(length_sampling)
 plt.tight_layout
 plt.show()
